7aab11b318b1ef628d052dfad84b9e396b137068
cedricbreuleux <cedric.breuleux@epitech.eu>
2025-03-31 15:39:10 +0200

    feat: adding controller base

--- src/main/java/fr/eni/projet/eniencheres/EniEncheresApplication.java
+++ src/main/java/fr/eni/projet/eniencheres/EniEncheresApplication.java
@@ -2,19 +2,14 @@ package fr.eni.projet.eniencheres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EniEncheresApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EniEncheresApplication.class, args);
     }
-
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
-    };
 }
